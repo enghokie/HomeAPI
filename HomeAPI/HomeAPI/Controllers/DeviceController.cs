d382b96d3bae839d741666d20a9518c2fd896759
Brian Kaplan <bakaplan2@gmail.com>
2015-02-25 00:10:30 -0500

    Draft API
    This is a mockup of basic GET and POST for a device resource.

---
+++ HomeAPI/HomeAPI/Controllers/DeviceController.cs
@@ -0,0 +1,36 @@
+﻿using HomeAPI.Models;
+using HomeAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HomeAPI.Controllers
+{
+    public class DeviceController : ApiController
+    {
+
+        private DeviceRepository deviceRepository;
+
+        public DeviceController()
+        {
+            this.deviceRepository = new DeviceRepository();
+        } 
+
+        public Device[] Get()
+        {
+            return deviceRepository.GetAllDevices();
+        }
+
+        public HttpResponseMessage Post(Device device)
+        {
+            this.deviceRepository.SaveDevice(device);
+
+            var response = Request.CreateResponse<Device>(System.Net.HttpStatusCode.Created, device);
+
+            return response;
+        }
+    }
+}
---
+++ HomeAPI/HomeAPI/Services/DeviceRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HomeAPI.Models;
+
+namespace HomeAPI.Services
+{
+    public class DeviceRepository
+    {
+        private const string CacheKey = "DeviceStore";
+
+        public DeviceRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var devices = new Device[]{}; //Load the devices from the back end data structure??
+
+            ctx.Cache[CacheKey] = devices;
+                }
+            }
+        }
+
+        public Device[] GetAllDevices()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Device[])ctx.Cache[CacheKey];
+            }
+
+            return new Device[]
+            {
+            new Device
+                {
+                    Id = 0,
+                    Name = "Placeholder"
+                }
+            };
+        }
+
+        public bool SaveDevice(Device device)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Device[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(device);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
