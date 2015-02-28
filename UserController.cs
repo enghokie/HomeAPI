1fad7cc6398f21ad8408b78a29ff10bc3f27367a
enghokie <enghokie@users.noreply.github.com>
2015-02-27 22:57:43 -0800

    Create UserController.cs

---
+++ UserController.cs
@@ -0,0 +1,45 @@
+using HomeAPI.Models;
+using HomeAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HomeAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+
+        private UserRepository userRepository;
+
+        public UserController()
+        {
+            this.userRepository = new UserRepository();
+        }
+
+        public User[] Get()
+        {
+            return userRepository.GetAllUsers();
+        }
+
+        public HttpResponseMessage Post(User user)
+        {
+            this.userRepository.SaveUser(user);
+
+            var response = Request.CreateResponse<User>(System.Net.HttpStatusCode.Created, user);
+
+            return response;
+        }
+
+        public HttpResponseMessage Delete(User user)
+        {
+            this.userRepository.DeleteUser(user);
+
+            var response = Request.CreateResponse<User>(System.Net.HttpStatusCode.OK, user);
+
+            return response;
+        }
+    }
+}
